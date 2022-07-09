053a96b477fc1476bde02ecf43112b02cacc4b15
Brian Mong <brian.mong9@gmail.com>
2022-07-08 22:23:37 -0700

    Created Places index page

---
+++ views/places/index.jsx
@@ -0,0 +1,20 @@
+// Index page for places
+const React = require('react');
+const Default = require('../default');
+
+function Index () {
+    return (
+        <Default>
+            <html>
+                <head>
+                    <title>Title</title>
+                </head>
+                <body>
+                    <h1>Places Index</h1>
+                </body>
+            </html>
+        </Default>
+    )
+}
+
+module.exports = Index
