92bf6688eec712072f5464aed5d0db4979864bad
Brian Mong <brian.mong9@gmail.com>
2022-07-22 20:51:19 -0700

    Created script for seed data

---
+++ seeders/seed-places.js
@@ -0,0 +1,26 @@
+// Used as a script to populate the database with mock info to test the app. process.exit closes out the script so it is not hung open
+const db = require('../models')
+
+db.Place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/h-thai-ml-tables.jpg',
+    founded: 1989
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-cat.jpg',
+    founded: 2020
+}])
+.then(() => {
+    console.log('Success!')
+    process.exit()
+})
+.catch(err => {
+    console.log('Failure!', err)
+    process.exit()
+})
